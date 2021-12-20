849379233afc98b71b464f075b7d095209424596
dadaroce <danielcediel97@gmail.com>
2021-12-20 10:20:50 -0500

    [FEAT]: update rplidar driver cpp to build with release flag

--- launch/rplidar_s1.launch.py
+++ launch/rplidar_s1.launch.py
@@ -5,7 +5,7 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(
-            node_name='rplidar_composition',
+            name='rplidar_composition',
             package='rplidar_ros',
             executable='rplidar_composition',
             output='screen',
